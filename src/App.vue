04db8be98b9f95d94ca57d24ceb02df3fed6b600
Alex Seeley <alexseeley134@gmail.com>
2024-06-27 11:42:12 -0600

    Implement tailwind and fix bugs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -11,6 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <p class="text-red-500">Hey</p>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
