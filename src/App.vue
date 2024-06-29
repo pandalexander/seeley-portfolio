e7bd6043061a909a82a3f6ad1257ff133c0d3cad
Alex Seeley <alexseeley134@gmail.com>
2024-06-28 23:10:24 -0600

    Create and style Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
 </script>
 
 <template>
-  <div>
+  <div class="pl-5 pr-5 overflow-hidden">
     <Header />
+    <main class="max-w-2xl mx-auto p-4">
+      <Hero />
+    </main>
   </div>
 </template>
 
---
+++ src/components/Hero.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <h1 class="text-2xl">
+      Making the web a better place, one
+      <span class="italic text-emerald-600">&lt;div&gt;</span> at a time.
+    </h1>
+  </section>
+</template>
