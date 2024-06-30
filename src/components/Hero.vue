1123c2d1d1806df4d1253be39d143c3ab8249ac1
Alex Seeley <alexseeley134@gmail.com>
2024-06-29 21:41:45 -0600

    Delete image from Hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,10 +4,5 @@
       Making software better, one
       <span class="italic text-emerald-600">&gt;program</span> at a time.
     </h1>
-    <img
-      src="/src/assets/main.png"
-      alt="a panda typing on a laptop"
-      class="max-w-80 h-1/5 object-cover"
-    />
   </section>
 </template>
