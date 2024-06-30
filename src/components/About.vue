41eeec93f09f7a6d8fe43107a53d63aee9297e44
Alex Seeley <alexseeley134@gmail.com>
2024-06-29 22:31:45 -0600

    Minor styling changes to about component

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,14 +7,14 @@
       I'm thrilled to create simple solutions across various platforms and
       technologies.
     </p>
-    <h2>Skills</h2>
+    <h2 class="pt-6 pb-3">Skills</h2>
     <div class="flex items-center justify-between">
       <div class="flex-grow"><hr /></div>
       <h3 class="mx-4 text-secondary">Frontend</h3>
       <div class="flex-grow"><hr /></div>
     </div>
     <p class="text-center">
-      JavaScript, HTML, CSS, Vue.js, Tailwind CSS, React
+      JavaScript, HTML, CSS, Vue.js, Tailwind&nbsp;CSS, React
     </p>
     <div class="flex items-center justify-between mt-9">
       <div class="flex-grow"><hr /></div>
@@ -33,6 +33,6 @@
       <h3 class="mx-4 text-secondary">Other</h3>
       <div class="flex-grow"><hr /></div>
     </div>
-    <p class="text-center">Git, Webpack, Bootstrap, Raspberry Pi</p>
+    <p class="text-center">Git, Webpack, Bootstrap, Raspberry&nbsp;Pi</p>
   </div>
 </template>
