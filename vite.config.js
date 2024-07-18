2f4c8f6b80ffe35154f0654185bf9fc83f527f58
Alex Seeley <alexseeley134@gmail.com>
2024-07-18 11:46:07 -0600

    Fix vite config to base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: "/seeley-portfolio/",
+  base: "/",
 });
